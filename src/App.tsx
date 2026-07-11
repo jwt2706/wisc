62bf564c71085bfd056ccf5eb0cbd66bb5f28dfc
jwt2706 <github@jthome.net>
2026-07-11 07:06:52 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        Vite + React + Tailwind v4 🚀
+      </h1>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+import App from './App'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element #root not found')
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
