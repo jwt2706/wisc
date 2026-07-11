315810c62c4943401792eb5b177a09782a1a8024
jwt2706 <jwt2home@gmail.com>
2026-07-11 12:34:04 -0400

    set root

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wisc/',
   plugins: [react(), tailwindcss()],
   envPrefix: ['VITE_', 'ELEVENLABS_'],
 })
